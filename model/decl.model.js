0dbed7280f6f3e6303c0129b0b1fcc57630fb606
Vitalino1978-stud <vitalinos1978@gmail.com>
2021-09-24 00:16:14 +0300

    adds schema declaration

--- model/decl.model.js
+++ model/decl.model.js
@@ -1,14 +1,36 @@
 const { Schema, model } = require('mongoose');
 
 const decl = new Schema({
-    number: {
+    declNumber: {
         type: String,
         required: true,
     },
-    date: {
+    declDateFrom: {
         type: String,
         required: true
+    },
+
+    declDateTo: {
+        type: String,
+        required: true
+    },
+
+    proformNumber: {
+        type: Number,
+        required: true
+    },
+
+    ammount: {
+        type: Number,
+        required: true
     }
+
+    // will connected with entity collection on MongoDB
+    // aritcul: {
+    //     type: String,
+    //     required: true
+    // }
+
 });
  
 const Declaration = model('declaration', decl)
